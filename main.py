637ab81b0dba27921aac9ccafb76ed4e7b714302
huyanhdo <86727493+huyanhdo@users.noreply.github.com>
2023-02-01 13:42:36 +0700

    fix api

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import math
 from fastapi import FastAPI, Query
 import numpy as np
 import pickle
-
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import json
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
 from lightgbm import LGBMRegressor
@@ -12,7 +14,23 @@ from xgboost import XGBRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 
+class Data(BaseModel):
+    Area:str
+    Number_of_bedroom:str
+    Type_of_house:str
+    Useable_area:str
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 linear = pickle.load(open('./model/linear.pickle','rb'))
 lasso = pickle.load(open('./model/lasso.pickle','rb'))
@@ -24,7 +42,8 @@ xgb = pickle.load(open('./model/xgb.pickle','rb'))
 dt = pickle.load(open('./model/decision_tree.pickle','rb'))
 rf = pickle.load(open('./model/random_forest.pickle','rb'))
 
-def type_of_house(x):
+def handle_type_of_house(x):
+    # x = str(x,'utf-8')
     if x == 'Nhà biệt thự':
         return 3.0
     if x== 'Nhà phố liền kề':
@@ -36,21 +55,25 @@ def type_of_house(x):
     return np.nan
 
 def preprocessing(features):
+    features = json.loads(features)
     area = math.log(float(features['Area']))
     number_of_bed_room = float(features['Number_of_bedroom'])
-    type_of_house =  type_of_house(features['Type_of_house'])
+    type_of_house =  handle_type_of_house(features['Type_of_house'])
     useable_area = math.log(float(features['Useable_area']))
-    return np.asarray([area,number_of_bed_room,type_of_house,useable_area])
+    fea = np.asarray([[area,number_of_bed_room,type_of_house,useable_area]])
+    return fea
 
 @app.get("/")
 def root():
     return {"Hello": "Data Science"}
 
-@app.get('/model')
-def predict(feature):
-    feature = preprocessing(feature)
-    pred = dt.predict(feature)
-    return math.e**pred
+@app.post('/model')
+def predict(feature:Data):
+    feature = preprocessing(feature.json())
+    # print(feature)
+    pred = dt.predict(feature)[0]
+    res = math.e**pred
+    return res
 
 # @app.get("/linear_regression/")
 # def linear_regression(features: List[str] = Query(None)):
